570310776b9cdea40a0666fc44a92eed58b86487
Александр Максимов <dev3@sviteco.com>
2023-01-09 15:58:29 +0200

    Initial commit

---
+++ src/main/java/ua/miceVsCats/MiceVsCatsApplication.java
@@ -0,0 +1,13 @@
+package ua.miceVsCats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiceVsCatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiceVsCatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/miceVsCats/MiceVsCatsApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.miceVsCats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiceVsCatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
